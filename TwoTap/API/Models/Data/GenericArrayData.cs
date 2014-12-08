01aac2a8b392f77c7278dc20babc449df0c2c27e
Jeremy Frederick <jeremy.frederick@reticentmedia.com>
2014-12-08 08:39:56 -0500

    Added helper to initialize generic array data with single item.

--- TwoTap/API/Models/Data/GenericArrayData.cs
+++ TwoTap/API/Models/Data/GenericArrayData.cs
@@ -9,5 +9,16 @@ namespace TwoTap.API.Models.Data
     public class GenericArrayData<T>
     {
         public IList<T> Data { get; set; }
+
+        public GenericArrayData()
+        {
+
+        }
+
+        public GenericArrayData(T data)
+        {
+            Data = new List<T>();
+            Data.Add(data);
+        }
     }
 }
